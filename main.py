baf6c5a925d3d8c0911cd05c4739bb4453ee02ec
Samuel Lee <102779687@students.swinburne.edu.my>
2025-09-17 11:50:53 +0800

    reinstall venv and fix import serial library issue. Awaiting for further testing

---
+++ main.py
@@ -0,0 +1,40 @@
+from serial_utils import (
+    connect_to_serial,
+    send_command,
+    enter_enable_mode,
+    enter_config_mode,
+    logout,
+    close_serial,
+)
+
+
+def main():
+    # (Linux: /dev/ttyUSB0, Mac: /dev/cu.usbserial, Windows: COM3)
+    port = "/dev/ttyUSB0"
+    baudrate = 9600
+
+    # Connect to the serial port
+    ser = connect_to_serial(port, baudrate)
+    if not ser:
+        return
+
+    # Enter enable mode
+    print("[*] Entering enable mode...")
+    output = enter_enable_mode(ser)
+    print(output)
+
+    # Enter config mode
+    print("[*] Entering config mode...")
+    output = enter_config_mode(ser)
+    print(output)
+
+    # Example command
+    response = send_command(ser, "show run")
+    print(response)
+
+    logout(ser)
+    close_serial(ser)
+
+
+if __name__ == "__main__":
+    main()
