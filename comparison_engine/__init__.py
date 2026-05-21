4a3cbb90ffe4c7c44df4a3752e72db549205f1be
Samuel Lee <102779687@students.swinburne.edu.my>
2026-05-22 07:22:01 +0800

    Add __init__.py and py.typed files to mark comparison_engine as a package and support type checking

---
+++ comparison_engine/__init__.py
@@ -0,0 +1,5 @@
+"""Mark comparison_engine as a Python package.
+
+This file lets code import modules from this directory with
+``import comparison_engine`` or ``from comparison_engine import ...``.
+"""
